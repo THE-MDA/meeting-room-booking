0c15743799392ae5cac2e8479cef891bda18e843
THE-MDA <dmitrymilchakov@mail.ru>
2026-04-02 13:34:45 +0300

    update config, logger and migrator

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,69 +1,69 @@
 package config
 
 import (
-    "fmt"
-    "os"
-    "strconv"
-    "time"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 type Config struct {
-    DBHost     string
-    DBPort     int
-    DBUser     string
-    DBPassword string
-    DBName     string
-    
-    JWTSecret     string
-    JWTExpiration time.Duration
-    
-    ServerPort string
-    
-    Environment string
-    LogLevel    string
+	DBHost     string
+	DBPort     int
+	DBUser     string
+	DBPassword string
+	DBName     string
+
+	JWTSecret     string
+	JWTExpiration time.Duration
+
+	ServerPort string
+
+	Environment string
+	LogLevel    string
 }
 
 func Load() (*Config, error) {
-    cfg := &Config{
-        DBHost:     getEnv("DB_HOST", "localhost"),
-        DBPort:     getEnvAsInt("DB_PORT", 5432),
-        DBUser:     getEnv("DB_USER", "app"),
-        DBPassword: getEnv("DB_PASSWORD", "secret"),
-        DBName:     getEnv("DB_NAME", "meeting_rooms"),
-        
-        JWTSecret:     getEnv("JWT_SECRET", "super-secret-jwt-key"),
-        JWTExpiration: time.Duration(getEnvAsInt("JWT_EXPIRATION_MINUTES", 30)) * time.Minute,
-        
-        ServerPort:  getEnv("SERVER_PORT", "8080"),
-        Environment: getEnv("ENVIRONMENT", "development"),
-        LogLevel:    getEnv("LOG_LEVEL", "info"),
-    }
-    
-    return cfg, nil
+	cfg := &Config{
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnvAsInt("DB_PORT", 5432),
+		DBUser:     getEnv("DB_USER", "app"),
+		DBPassword: getEnv("DB_PASSWORD", "secret"),
+		DBName:     getEnv("DB_NAME", "meeting_rooms"),
+
+		JWTSecret:     getEnv("JWT_SECRET", "super-secret-jwt-key"),
+		JWTExpiration: time.Duration(getEnvAsInt("JWT_EXPIRATION_MINUTES", 30)) * time.Minute,
+
+		ServerPort:  getEnv("SERVER_PORT", "8080"),
+		Environment: getEnv("ENVIRONMENT", "development"),
+		LogLevel:    getEnv("LOG_LEVEL", "info"),
+	}
+
+	return cfg, nil
 }
 
 func (c *Config) GetDBConnectionString() string {
-    return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-        c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName)
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName)
 }
 
 func (c *Config) GetDatabaseURL() string {
-    return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
-        c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
 }
 
 func getEnv(key, defaultValue string) string {
-    if value := os.Getenv(key); value != "" {
-        return value
-    }
-    return defaultValue
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
 
 func getEnvAsInt(key string, defaultValue int) int {
-    if value := os.Getenv(key); value != "" {
-        if intValue, err := strconv.Atoi(value); err == nil {
-            return intValue
-        }
-    }
-    return defaultValue
+	if value := os.Getenv(key); value != "" {
+		if intValue, err := strconv.Atoi(value); err == nil {
+			return intValue
+		}
+	}
+	return defaultValue
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,40 +1,40 @@
 package logger
 
 import (
-    "log/slog"
-    "os"
+	"log/slog"
+	"os"
 )
 
 func Init(level slog.Level) {
-    var handler slog.Handler
-    
-    if os.Getenv("ENVIRONMENT") == "production" {
-        handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-            Level:     level,
-            AddSource: true,
-        })
-    } else {
-        handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-            Level:     level,
-            AddSource: true,
-        })
-    }
-    
-    slog.SetDefault(slog.New(handler))
+	var handler slog.Handler
+
+	if os.Getenv("ENVIRONMENT") == "production" {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level:     level,
+			AddSource: true,
+		})
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level:     level,
+			AddSource: true,
+		})
+	}
+
+	slog.SetDefault(slog.New(handler))
 }
 
 func Debug(msg string, args ...any) {
-    slog.Debug(msg, args...)
+	slog.Debug(msg, args...)
 }
 
 func Info(msg string, args ...any) {
-    slog.Info(msg, args...)
+	slog.Info(msg, args...)
 }
 
 func Warn(msg string, args ...any) {
-    slog.Warn(msg, args...)
+	slog.Warn(msg, args...)
 }
 
 func Error(msg string, args ...any) {
-    slog.Error(msg, args...)
+	slog.Error(msg, args...)
 }
